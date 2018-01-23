f3783e5c3dbaf9a794c1e35dc798654d887971ca
Dustin Quam <quamsta@gmail.com>
2018-01-23 10:37:08 -0600

    more testing

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -53,7 +53,7 @@ class Page_Controller extends ContentController {
 	public function init() {
 		parent::init();
 	$user = getenv('APACHE_RUN_USER');
-	// if(!$user) $user = getenv('USER');
+	if(!$user) $user = getenv('USER');
 	// if(!$user) $user = getenv('USERNAME');
 	// if(!$user && function_exists('posix_getuid')) {
 	// 	$userDetails = posix_getpwuid(posix_getuid());
